dd39f29cd5c6b5012eeec42173ab2f338eecde7a
DmitrijMK <dmitrijmk@gmail.com>
2018-06-20 09:53:07 +0300

    step 1

---
+++ js/game.js
@@ -0,0 +1,96 @@
+function game(setBombs = 10, fields = 100) {
+    let bombs = 0;
+    const subheader = document.getElementById('subheader');
+    subheader.innerHTML = `${fields} left to clear`;
+
+    /* Проверка на бомбу */
+    const searchBomb = id => document.getElementById(id) ?
+        ((document.getElementById(id)).hasAttribute('bomb') !== false ? 1 : 0) : 0;
+
+    /* Создание поля */
+    for (let i = 0; i < fields; i++) {
+        const field = document.createElement('div');
+        field.className = 'close';
+        field.id = `${Math.floor(i / 10)}_${i % 10}`;
+        document.getElementById('game').appendChild(field);
+    }
+
+    /* Расставляем мины */
+    function plantBombs() {
+        const collection = document.getElementsByClassName('close');
+
+        for (let i = 0; setBombs > bombs; i++) {
+            if (Math.random() < 0.1 && setBombs !== bombs && !collection[i].hasAttribute('bomb')) {
+                collection[i].setAttribute('bomb', 'bomb');
+                document.getElementById('header').innerHTML = (++bombs) + ' bombs';
+            }
+            if (i === collection.length - 1 && setBombs > bombs) i = 0;
+        }
+    }
+
+    plantBombs();
+
+    /*  */
+    for (let i = 0; i < fields; i++) {
+        const y = Math.floor(i / 10); //ось y
+        const x = i % 10;             //ось x
+        const collectionFields = document.getElementById(y + '_' + x);
+        let numberOfNearestBombs = 0;
+
+        // if (setBombs !== bombs && Math.random() > 0.7) {
+        //     collectionFields.setAttribute('bomb', 'bomb');
+        //     document.getElementById('header').innerHTML = (++bombs) + ' bombs';
+        // }
+
+        for (let i = 0; i < 9; i++) {
+            numberOfNearestBombs += searchBomb(`${(y - (Math.floor(i / 3) - 1))}_${(x - (i % 3 - 1))}`);
+        }
+
+        collectionFields.innerHTML = numberOfNearestBombs === 0 ? ' ' : numberOfNearestBombs;
+
+        collectionFields.onclick = function () {
+            let mix = this.id.split('_');
+            openField(mix[0], mix[1]);
+        };
+
+        collectionFields.oncontextmenu = function () {
+            this.className = this.className.indexOf('flag') !== -1 ?
+                this.className.replace(/ flag/, '') : this.className + ' flag';
+            return false;
+        }
+    }
+
+    /* Открытие поля*/
+    function openField(y, x) {
+        const selectedField = document.getElementById(y + '_' + x);
+
+        if (!selectedField || selectedField.className.indexOf('close') === -1) return;
+
+        if (selectedField.hasAttribute('bomb') === false) {
+            const elements = table.getElementsByTagName('div');
+            let remainingCloseFields = 0;
+            selectedField.className = 'open';
+
+            //Открытие соседних ячеек
+            for (let elem of elements) {
+                if (elem.className && elem.className.indexOf('close') !== -1) remainingCloseFields++;
+            }
+            subheader.innerHTML = `${remainingCloseFields} left to clear`;
+
+            if (remainingCloseFields <= bombs) subheader.innerHTML = 'You win!';
+        } else {
+            const collection = document.getElementsByTagName('div');
+
+            for (let i = 0; i < collection.length; i++) {
+                collection[i].className = collection[i].hasAttribute('bomb') !== false ? 'bomb' : 'open';
+            }
+            subheader.innerHTML = 'You lose!';
+        }
+
+        if (selectedField.innerHTML === ' ') {
+            for (let i = 0; i < 9; i++) {
+                openField(y - ((Math.floor(i / 3) - 1)), x - (((i % 3) - 1)));
+            }
+        }
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,64 +1,21 @@
-function renderTable(quantityRow = 10, quantityCell = 10, width = 28, height = 28) {
-    const test = document.querySelector('.createElementButton');
-
-    if (test) {
-        const table = document.querySelector('.game');
-        table.remove();
-        test.remove();
-    }
-
-    const tableDiv = document.querySelector('#table');
-    const table  = document.createElement('table');
-    const createElementButton = document.createElement('button');
-
-    for (let i = 0; i < quantityRow; i++) {
-        const tr = table.insertRow(0);
-
-        for (let j = 0; j < quantityCell; j++) {
-            const td = tr.insertCell(0);
-
-            td.style.border = '1px solid #777';
-            td.style.width  = width + 'px';
-            td.style.height = height + 'px';
-        }
-    }
-
-    createElementButton.innerHTML           = 'Поменять цвета';
-    table.style.borderCollapse = 'collapse';
-
-    table.addEventListener('click', e => {
-        if (e.target.tagName === 'TD') applyColor(e.target);
-    });
-
-    createElementButton.addEventListener('click', () => {
-        const allTd = table.getElementsByTagName('td');
-
-        for (let i = 0; i < allTd.length; i++) {
-            applyColor(allTd[i]);
-        }
-    });
-
-    table.setAttribute('class', 'game');
-    tableDiv.appendChild(table);
-    createElementButton.setAttribute('class', 'createElementButton');
-    tableDiv.appendChild(createElementButton);
-}
-
-function applyColor(value) {
-    if (value.className !== 'black') {
-        value.style.background = 'black';
-        value.className        = 'black';
-    } else {
-        value.style.background = 'white';
-        value.className        = 'white';
-    }
-}
-
-const startNewGameButton = document.querySelector('#startNewGame');
-startNewGameButton.addEventListener('click', () => renderTable());
-
-
-
+game();
+//
+// const field = document.getElementById('field');
+// const setBombs = document.getElementById('bombs');
+// const table = document.getElementById('table');
+// const button = document.getElementById('startNewGame');
+//
+// function render() {
+//     let div = document.getElementById('game');
+//     table.setAttribute('style', `width:${field.value * 3.2}px`);
+//     table.removeChild(div);
+//     let newGame = table.createElement('div');
+//     table.appendChild(newGame);
+//     newGame.setAttribute('id', 'game');
+// }
+//
+// // button.addEventListener('click', () => render());
+// button.addEventListener('click', () => game(setBombs.value, field.value));
 
 
 
